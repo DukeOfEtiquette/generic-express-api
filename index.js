2f8355f34a76ddadb6eaf98ad8d8a27d413b6e12
DukeOfEtiquette <duquetteadam@gmail.com>
2019-03-30 20:21:02 -0700

    Refactor server code into own folder to leave index.js thin

--- index.js
+++ index.js
@@ -2,17 +2,6 @@
 
 require('dotenv').config();
 
-const express = require('express');
-const morgan = require('morgan')(process.env.MORGAN_FORMAT);
+const server = require('./server/server.js');
 
-const app = express();
-
-app.use(morgan);
-
-app.get('/', (req, res, next) => {
-  res.status(200).send('Hello world');
-});
-
-app.listen(process.env.PORT, () => {
-  console.log(`Server listening on PORT ${process.env.PORT}`);
-});
+server.start(process.env.PORT);
