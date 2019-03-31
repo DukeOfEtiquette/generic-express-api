67d589da03028f9be7cee67b05988cdc42a3aff7
DukeOfEtiquette <duquetteadam@gmail.com>
2019-03-30 17:16:56 -0700

    Add morgan to server

--- index.js
+++ index.js
@@ -1,12 +1,18 @@
 'use strict';
 
+require('dotenv').config();
+
 const express = require('express');
+const morgan = require('morgan')(process.env.MORGAN_FORMAT);
+
 const app = express();
+
+app.use(morgan);
 
 app.get('/', (req, res, next) => {
   res.status(200).send('Hello world');
 });
 
-app.listen(3000, () => {
-  console.log('Server listening on PORT 3000');
+app.listen(process.env.PORT, () => {
+  console.log(`Server listening on PORT ${process.env.PORT}`);
 });
