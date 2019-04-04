2511cd0a5725db9f81ce55e2f516a0ed4aff8fb2
DukeOfEtiquette <duquetteadam@gmail.com>
2019-04-04 16:09:27 -0700

    Add swagger-jsdoc module, document /helloworld route and test output just using swagger editor

--- server/app.js
+++ server/app.js
@@ -6,6 +6,7 @@ Third Party Libraries
 const express = require('express');
 const morgan = require('morgan');
 const cors = require('cors');
+const swaggerJSDoc = require('swagger-jsdoc');
 
 
 /********************************************************************************
@@ -20,6 +21,32 @@ Middleware
 app.use(cors());
 app.use(morgan(process.env.MORGAN_FORMAT));
 
+const swaggerDefinition = {
+  info: {
+    title: 'Hello World', 
+    version: '1.0.0', 
+    description: 'A generic Express API',
+  },
+  host: `localhost:${process.env.PORT}`,
+  basePath: '/',
+};
+
+const options = {
+  // Import swaggerDefinitions
+  swaggerDefinition,
+  // Path to the API docs
+  // Note that this path is relative to the current directory from which the Node.js is ran, not the application itself.
+  apis: ['./server/api/v1.js'],
+};
+
+// Initialize swagger-jsdoc -> returns validated swagger spec in json format
+const swaggerSpec = swaggerJSDoc(options);
+
+// Serve swagger docs the way you like (Recommendation: swagger-tools)
+app.get('/api-docs.json', (req, res) => {
+  res.setHeader('Content-Type', 'application/json');
+  res.send(swaggerSpec);
+});
 
 /********************************************************************************
 Routes
