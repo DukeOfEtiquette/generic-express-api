41fdb37aee5e9bafb6659463893712642fcad0f5
DukeOfEtiquette <duquetteadam@gmail.com>
2019-04-04 15:37:38 -0700

    Add testing framework with Jest with Hello World test, add jsdocs

--- __test__/api/api.test.js
+++ __test__/api/api.test.js
@@ -5,11 +5,11 @@ const request = require('supertest');
 const app = require('../../server/app.js').expressApp;
 
 describe('/api/v1', () => {
-  describe('#root', () => {
-    describe('#GET', () => {
+  describe('/helloworld', () => {
+    describe('GET', () => {
       it('should return status 200 with body "Hello world v1"', (done) => {
         request(app)
-          .get('/')
+          .get('/helloworld')
           .expect(200)
           .expect('Hello world v1', done);
       });
